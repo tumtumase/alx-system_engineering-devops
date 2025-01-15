b1174bfbc52cb657aa8e3518c037037b02e445a4
tumtumase <moecabolerato@gmail.com>
2025-01-15 15:01:49 -0800

    trying my best

--- 0x0A-configuration_management/1-install_a_package.pp
+++ 0x0A-configuration_management/1-install_a_package.pp
@@ -1,6 +1,5 @@
-# Install Flask from pip3 with specific version
-
-package { 'Flask':
-  ensure   => '2.1.0',
-  provider => 'pip3',
+exec { 'install_flask':
+  command => '/usr/bin/pip3 install flask==2.1.0',
+  path    => ['/usr/bin', '/bin'],
+  unless  => '/usr/bin/pip3 show flask | grep -q "Version: 2.1.0"',
 }
